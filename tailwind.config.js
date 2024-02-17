1b70f5c20cb82ec333e5c67c7c2972437519a83e
JadiDapa <daffafalthaf25@gmail.com>
2024-02-17 17:36:34 +0700

    feat (hero-section): Implementing base design and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFF6DA",
+        secondary: "",
+        tertiary: "",
+      },
+    },
   },
   plugins: [],
 };
